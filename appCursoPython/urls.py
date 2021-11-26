853f977711679c9dd111f4869f5eec8d7b803632
CristianAlexisZamora <72317447+CristianAlexisZamora@users.noreply.github.com>
2021-11-25 18:41:47 -0600

    Actualizacion de teoria de django

--- appCursoPython/urls.py
+++ appCursoPython/urls.py
@@ -5,8 +5,8 @@ from appCursoPython import views
 
 urlpatterns = [
     path('POO/', views.pythonPOO, name="POO"),
-    path('cursoDjango/', views.frameworkDjango, name="FrameworkDjango"),
-    path('ejercicioDjango/', views.ejercicioDjango, name="EjercicioDjango"),
+    path('cursoDjango/', views.cursoDjango01, name="FrameworkDjango"),
+    path('ejercicioDjango/', views.cursoDjango02, name="EjercicioDjango"),
     path('libros/', views.listado_libros, name="Libros"),
     path('videosComplementarios/', views.videosComplementarios, name="VideosComplementarios"),
     path('operadores/', views.operadores, name="Operadores"),
