651d4cb6475769c00a7d3a975b8e3a909f7e64ab
CristianAlexisZamora <72317447+CristianAlexisZamora@users.noreply.github.com>
2021-12-05 22:17:26 -0600

    Se ordenaron las vistas del curso de python por modulos, se agregaron algunas vistas, urls faltantes y se realizaron ajustes a templates que no se mostrabanSe ordenaron las vistas del curso de python por modulos, se agregaron algunas vistas, urls faltantes y se realizaron ajustes a templates que no se mostraban

--- appCursoPython/views.py
+++ appCursoPython/views.py
@@ -1,14 +1,62 @@
 from django.shortcuts import render
 from appCursoPython.models import practica
-# Create your views here.
-# Vista creada para la pagina de POO en python
+
+#--------------------Modulo 1 ------------------------#
+#Vista para el tutorial de introduccion
+def introduccion(request):
+    return render(request, "appCursoPython/introduccion.html")
+
+#Vista para el tutorial de tipos de datos
+def variables(request):
+    return render(request, "appCursoPython/variables.html")
+
+#Vista para el tutorial de variables
+def tiposDatos(request):
+    return render(request, "appCursoPython/tiposDatos.html")
+
+# Vista creada para el tutorial de Operadores
+def operadores(request):
+    return render(request, "appcursoPython/operadores.html")
+   
+
+#------------------- Modulo 2 ------------------------#
+# Vista creada para el tutorial de sentencia if
+def sentenciaIf(request):
+    return render(request, "appcursoPython/sentenciaIf.html")
+
+# Vista creada para el tutorial de sentencia if
+def bucleWhile(request):
+    return render(request, "appcursoPython/bucleWhile.html")
+
+
+#------------------ Modulo 3 -------------------------#
+# Vista creada para el tutorial de tipoRange
+def tipoRange(request):
+    return render(request, "appCursoPython/tipoRange.html")
+# Vista creada para el tutorial de tipoSet
+def tipoSet(request):
+    return render(request, "appCursoPython/tipoSet.html")
+# Vista creada para el tutorial de tipoDict
+def tipoDict(request):
+    return render(request, "appCursoPython/tipoDict.html")
+# Vista creada para el tutorial de tipoStr
+def tipoStr(request):
+    return render(request, "appCursoPython/tipoStr.html")
+
+
+#----------------- Modulo 4 ---------------------------#
+# Vista creada para el tutorial de funciones en python
+def funciones(request):
+    return render(request, "appCursoPython/funciones.html")
+# Vista creada para el tutorial de Espacios de nombres, modulos y paquetes
+def espacioNombres(request):
+    return render(request, "appCursoPython/Espaciosnmp.html")
+# Vista creada para el tutorial de programacion orientada a objetos
 def pythonPOO(request):
     return render(request, "appCursoPython/pythonPOO.html")
 
-# Vista creada para el tutorial de inicio
-def inicio(request):
-    return render(request, "appCursoPython/inicio.html") 
 
+#----------------Django---------------------------------#
 # Vistas creadas para el curso del framework Django
 def cursoDjango01(request):
     return render(request, "django/cursoDjango01.html")
@@ -22,6 +70,13 @@ def cursoDjango03(request):
 def cursoDjango04(request):
     return render(request, "django/cursoDjango04.html")
 
+
+
+#------------------ Otras vistas ------------------------#
+# Vista creada para el tutorial de inicio
+def inicio(request):
+    return render(request, "appCursoPython/inicio.html") 
+
 # Vista creada para el listado de libros que se posee
 def listado_libros(request):
     return render(request, "appCursoPython/libros.html")
@@ -34,43 +89,7 @@ def videosComplementarios(request):
 def comunidad (request):
     return render(request, "appCursoPython/comunidad.html")
 
-# Vista creada para el tutorial de Operadores
-def operadores(request):
-    return render(request, "appcursoPython/operadores.html")
-   
-# Vista creada para el tutorial de sentencia if
-def sentenciaIf(request):
-    return render(request, "appcursoPython/sentenciaIf.html")
-
-# Vista creada para el tutorial de sentencia if
-def bucleWhile(request):
-    return render(request, "appcursoPython/bucleWhile.html")
-
 #Vista creada para la práctica
 def practicapage(request, modulo):
     resultado = practica.objects.get(idpractica=modulo)
     return render(request, "appcursoPython/practica.html", {"modulo":resultado.idpractica,"tema":resultado.tema, "pregunta1":resultado.pregunta1, "pregunta2":resultado.pregunta2, "pregunta3":resultado.pregunta3, "res1":resultado.respuesta1, "res2":resultado.respuesta2, "res3":resultado.respuesta3})
-
-#Modulo 3
-
-# Vista creada para el tutorial de tipoRange
-def tipoRange(request):
-    return render(request, "appCursoPython/tipoRange.html")
-# Vista creada para el tutorial de tipoSet
-def tipoSet(request):
-    return render(request, "appCursoPython/tipoSet.html")
-# Vista creada para el tutorial de tipoDict
-def tipoDict(request):
-    return render(request, "appCursoPython/tipoDict.html")
-
-#Vista para el tutorial de introduccion
-def introduccion(request):
-    return render(request, "appCursoPython/introduccion.html")
-
-#Vista para el tutorial de tipos de datos
-def variables(request):
-    return render(request, "appCursoPython/variables.html")
-
-#Vista para el tutorial de variables
-def tiposDatos(request):
-    return render(request, "appCursoPython/tiposDatos.html")
