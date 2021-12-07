2c1bf5aa4be9320d73825414b04993c47db0b276
donotr <tr19005@ues.edu.sv>
2021-12-07 01:36:05 -0600

    donitocambios

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from appCursoPython.models import nota
+from main.user.models import User
 
 # Create your views here.
 
@@ -12,8 +14,50 @@ def academiaCurso(request):
 
 #Vista de mi cuenta
 def cuenta(request):
-    return render(request, "main/cuenta.html")
+
+    iduser= User.objects.get(id=request.user.id)
+    notasUser= nota.objects.filter(id_usuario = iduser)
+    modulo1=0
+    modulo2=0
+    modulo3=0
+    modulo4=0
+
+
+
+    for registro in notasUser:
+        if (registro.nota_practica < 5):
+            modulo1 += registro.nota_practica
+        elif(registro.nota_practica < 8):
+            modulo2 += registro.nota_practica
+        elif(registro.nota_practica < 14):
+            modulo3 += registro.nota_practica
+        else:
+            modulo4 += registro.nota_practica
+
+    modulo1/= 4
+    modulo2/= 3
+    modulo3/= 6
+    modulo4/= 6
+
+
+    contexto = { 
+    "Nombre": request.user.first_name,
+    "Apellido": request.user.last_name,
+    "Edad": request.user.edad,
+    "Sexo": request.user.sexo,
+    "Usuario":request.user.username,
+    "Modulo1":modulo1,
+    "Modulo2":modulo2,
+    "Modulo3":modulo3,
+    "Modulo4":modulo4,
+    }
+
+
+
+    return render(request, "main/cuenta.html",contexto)
+
 
 #Vista de acerca de nosotros
-def nosotros(request):
+def nosotros(request): 
     return render(request, "main/nosotros.html")
+
